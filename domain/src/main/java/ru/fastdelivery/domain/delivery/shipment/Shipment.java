3dc5e3d336444117d82266a6afc19b173e2b2237
Anton Zarubin <a-zarubin@mail.ru>
2024-10-03 21:28:48 +0300

    add logging

--- domain/src/main/java/ru/fastdelivery/domain/delivery/shipment/Shipment.java
+++ domain/src/main/java/ru/fastdelivery/domain/delivery/shipment/Shipment.java
@@ -1,5 +1,6 @@
 package ru.fastdelivery.domain.delivery.shipment;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.fastdelivery.domain.common.currency.Currency;
 import ru.fastdelivery.domain.common.weight.Weight;
 import ru.fastdelivery.domain.delivery.pack.Pack;
@@ -10,13 +11,17 @@ import java.util.List;
  * @param packages упаковки в грузе
  * @param currency валюта объявленная для груза
  */
+
+@Slf4j
 public record Shipment(
         List<Pack> packages,
         Currency currency
 ) {
     public Weight weightAllPackages() {
-        return packages.stream()
+        Weight totalWeight = packages.stream()
                 .map(Pack::weight)
                 .reduce(Weight.zero(), Weight::add);
+        log.info("Cargo weight is {} grams", totalWeight.weightGrams());
+        return totalWeight;
     }
 }
--- useCase/src/main/java/ru/fastdelivery/usecase/TariffCalculateUseCase.java
+++ useCase/src/main/java/ru/fastdelivery/usecase/TariffCalculateUseCase.java
@@ -1,11 +1,13 @@
 package ru.fastdelivery.usecase;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.fastdelivery.domain.common.price.Price;
 import ru.fastdelivery.domain.delivery.shipment.Shipment;
 
 import javax.inject.Named;
 
+@Slf4j
 @Named
 @RequiredArgsConstructor
 public class TariffCalculateUseCase {
@@ -15,10 +17,11 @@ public class TariffCalculateUseCase {
         var weightAllPackagesKg = shipment.weightAllPackages().kilograms();
         var minimalPrice = weightPriceProvider.minimalPrice();
 
-        return weightPriceProvider
+        Price priceByWeight = weightPriceProvider
                 .costPerKg()
-                .multiply(weightAllPackagesKg)
-                .max(minimalPrice);
+                .multiply(weightAllPackagesKg);
+        log.info("Price calculated by weight: {}", priceByWeight.amount());
+        return priceByWeight.max(minimalPrice);
     }
 
     public Price minimalPrice() {
